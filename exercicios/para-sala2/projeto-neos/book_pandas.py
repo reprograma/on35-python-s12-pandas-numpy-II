26c3ec7a1b8e3a281ae7f2c241807f24049bc5c6
Jamile Barroso <jamilebarroso0@gmail.com>
2024-08-22 21:54:44 -0300

    projeto neos

--- exercicios/para-sala2/projeto-neos/book_pandas.py
+++ exercicios/para-sala2/projeto-neos/book_pandas.py
@@ -1,18 +1,50 @@
 # %%
 import pandas as pd
 # %%
-dados = pd.read_csv('book.csv',sep=';')
+dados = pd.read_csv('book.csv',sep=';', skipinitialspace = True)
 dados
 # %%
 df = pd.DataFrame(dados)
 # %%
 df.head()
 # %%
-df.isnull().sum()
+df.info()
 # %%
-df = df[['COD_CLIENTE','CLIENTE','PORTFOLIO','DATAPOS','CATEGORIA','PAPEL','PATRIM_LIQUIDO']]
+colunas = ['COD_CLIENTE',
+           'CLIENTE',
+           'DATAPOS',
+           'CATEGORIA',
+           'CODIGO',
+           'PAPEL',
+           'QTDE_TOTAL',
+           'PRECO',
+           'VALOR_MERC_LIQUIDO']
+df = df[colunas]
+df
+# %% 
+df = df.rename(columns={"DATAPOS":"DATA_BOOK",
+                        "QTDE_TOTAL":"QTDE_COTAS",
+                        "PRECO":"VALOR_COTA",
+                        "VALOR_MERC_LIQUIDO":"VALOR/PL"
+                        })
 df
 # %%
-df['PAPEL'] = df['PAPEL'].fillna('---')
+df['CLIENTE'] = df['CLIENTE'].replace('NÉOS PREV. COMPLEMENTAR - ','',regex=True)
+df['CLIENTE'] = df['CLIENTE'].replace('NÉOS - ','',regex=True)
+df['CLIENTE'] = df['CLIENTE'].replace('NÉOS','NEOS',regex=True)
 df
+# %%
+df['COD_CLIENTE'] = df['COD_CLIENTE'].replace(' ','',regex=True)
+df['COD_CLIENTE']
+# %%
+df_pl_tesouraria = df[['COD_CLIENTE','CLIENTE','DATA_BOOK','CATEGORIA','VALOR/PL']]
+df_pl_tesouraria
+# %%
+pl_tesouraria = (df_pl_tesouraria['CATEGORIA'] == 'TESOURARIA') | (df_pl_tesouraria['CATEGORIA'] == 'PATRIMONIO LIQUIDO') 
+df_pl_tesouraria = df_pl_tesouraria[pl_tesouraria]
+# %%
+# %%
+df_pl_tesouraria.to_excel('BOOK_2008.xlsx')
+# %%
+df_pl_tesouraria.to_csv('BOOK_2008.csv')
 # %%
